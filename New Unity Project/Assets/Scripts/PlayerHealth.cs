4c361e00f775f6eef5487ac5b4d34ca986f32291
Jonas <jonas.illipe@elev.ga.lbs.se>
2022-02-07 16:04:32 +0100

    Health UI (HUD)

--- New Unity Project/Assets/Scripts/PlayerHealth.cs
+++ New Unity Project/Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int health;
+    public int maxHealth;
     int currentHealth;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     void Start()
     {
-        currentHealth = health;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < maxHealth)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
